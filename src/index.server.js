7a08c3443f0d7c8eb143f2781d32397f5b220f9a
Pankaj Godhani <pankajgodhani99@gmail.com>
2021-04-14 23:22:42 -0500

    cors

--- src/index.server.js
+++ src/index.server.js
@@ -3,7 +3,7 @@ const env = require('dotenv');
 const mongoose = require('mongoose');
 const app = express();
 const path = require('path')
-
+const cors = require('cors')
 //routes
 
 const authRoutes = require('./routes/auth');
@@ -32,6 +32,7 @@ mongoose.connect(
 
 //middleware
 
+app.use(cors())
 app.use(express.json());
 app.use('/public',express.static(path.join(__dirname,'uploads')));
 app.use('/api',authRoutes)
